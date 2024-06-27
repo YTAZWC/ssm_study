4adde8458a270cd967ae5c932db13cc321908102
yang <18570354653@163.com>
2024-06-27 15:16:36 +0800

    Spring MVC SSM整合(拦截器参数)

--- springmvc_study/springmvc_12_interceptor/src/main/java/top/ytazwc/springmvc/controller/interceptor/ProjectInterceptor.java
+++ springmvc_study/springmvc_12_interceptor/src/main/java/top/ytazwc/springmvc/controller/interceptor/ProjectInterceptor.java
@@ -1,6 +1,7 @@
 package top.ytazwc.springmvc.controller.interceptor;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,7 +19,9 @@ public class ProjectInterceptor implements HandlerInterceptor {
     // 请求前
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("preHandler ... ");
+        String contentType = request.getHeader("Content-Type");
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        System.out.println("preHandler ... " + contentType);
         return true;
     }
 
