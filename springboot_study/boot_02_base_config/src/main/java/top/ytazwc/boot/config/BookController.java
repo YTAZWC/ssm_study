01d612863cbd8f0b3f53d9239dab1cacb331b15d
yang <18570354653@163.com>
2024-06-28 15:34:01 +0800

    Spring Boot 基本配置(配置文件、日志级别)

---
+++ springboot_study/boot_02_base_config/src/main/java/top/ytazwc/boot/config/BookController.java
@@ -0,0 +1,22 @@
+package top.ytazwc.boot.config;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yt
+ * 2024/6/28
+ */
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @GetMapping("/{id}")
+    public String getById(@PathVariable Integer id) {
+        System.out.println("id ==> " + id);
+        return "Spring Boot!";
+    }
+
+}
