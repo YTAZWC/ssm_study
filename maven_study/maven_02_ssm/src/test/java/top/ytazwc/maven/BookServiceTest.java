fe3ca5f66dde18bc52cc24ee9d469aacc034cd9a
yang <18570354653@163.com>
2024-06-28 11:37:58 +0800

    maven 属性、多环境开发、跳过测试

---
+++ maven_study/maven_02_ssm/src/test/java/top/ytazwc/maven/BookServiceTest.java
@@ -0,0 +1,48 @@
+package top.ytazwc.maven;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import top.ytazwc.maven.config.SpringConfig;
+import top.ytazwc.maven.domain.Book;
+import top.ytazwc.maven.service.BookService;
+
+import java.util.List;
+
+/**
+ * @author yt
+ * 2024/6/26
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
+public class BookServiceTest {
+
+    @Autowired
+    private BookService bookService;
+
+    @Test
+    public void testGetById() {
+        Book book = bookService.getById(2);
+        System.out.println(book);
+    }
+
+    @Test
+    public void testGetAll() {
+        List<Book> all = bookService.getAll();
+        System.out.println(all);
+    }
+
+    @Test
+    public void testDelete() {
+        bookService.delete(2);
+    }
+
+    @Test
+    public void testUpdate() {
+        Book book = new Book(3, "百那些手小集术", "方杰", "你入万总民元满程业参类来京包日。及联济北毛力标容育听生眼。合存千号带族共置日即且所。省得加头八自安除科认飞业许院。");
+        bookService.update(book);
+    }
+
+}
