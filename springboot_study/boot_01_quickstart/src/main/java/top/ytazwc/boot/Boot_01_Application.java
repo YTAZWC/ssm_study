de0b69339e1fe9ff5957e08f1d38bad810d8adfe
yang <18570354653@163.com>
2024-06-28 14:14:06 +0800

    Spring Boot 入门案例初始工程

---
+++ springboot_study/boot_01_quickstart/src/main/java/top/ytazwc/boot/Boot_01_Application.java
@@ -0,0 +1,13 @@
+package top.ytazwc.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot_01_Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot_01_Application.class, args);
+    }
+
+}
