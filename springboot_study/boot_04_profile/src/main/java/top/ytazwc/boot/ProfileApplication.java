b48a62b1bd54323aface7926b7c56d02ea3ed72b
花木凋零成兰 <18570354653@163.com>
2024-06-28 23:25:57 +0800

    Spring Boot 多环境开发

--- springboot_study/boot_04_profile/src/main/java/top/ytazwc/boot/ProfileApplication.java
+++ springboot_study/boot_04_profile/src/main/java/top/ytazwc/boot/ProfileApplication.java
@@ -3,6 +3,9 @@ package top.ytazwc.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 花木凋零成兰
+ */
 @SpringBootApplication
 public class ProfileApplication {
 
