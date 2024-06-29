5b684778532a777ecad3fd7f4c64ac43b1f97c1f
yang <18570354653@163.com>
2024-06-29 10:33:36 +0800

    Spring Boot 多环境配置(maven设置环境与Boot设置环境优先级)

---
+++ springboot_study/boot_05_maven_and_boot_profile/src/main/java/top/ytazwc/boot/MavenAndBootProfileApplication.java
@@ -0,0 +1,13 @@
+package top.ytazwc.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenAndBootProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenAndBootProfileApplication.class, args);
+    }
+
+}
