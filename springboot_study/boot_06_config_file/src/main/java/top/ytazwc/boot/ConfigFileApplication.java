529b8686c9f92346839340fb655febe6cf1f8eef
yang <18570354653@163.com>
2024-06-29 11:31:20 +0800

    Spring Boot 配置文件分类

---
+++ springboot_study/boot_06_config_file/src/main/java/top/ytazwc/boot/ConfigFileApplication.java
@@ -0,0 +1,13 @@
+package top.ytazwc.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigFileApplication.class, args);
+    }
+
+}
